9f92d42b42695995672b732d4582b698b5636a9f
Alexander Shaposhnikov <ashaposhnikov@google.com>
2025-01-27 05:10:59 +0000

    Modify run_tests.sh

--- clang_pgo_build/run_tests.sh
+++ clang_pgo_build/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+export LD_LIBRARY_PATH=$HOME/my_llvm_fork/llvm-project/build/lib/x86_64-unknown-linux-gnu
 D=`realpath ../test_data/tests`
 echo "$D"
 taskset -c 0-7 ./contest/grader/contest-grader --threads 8 --tests $D

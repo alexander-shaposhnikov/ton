a6cd317e31921f92f2d8ca3dcfea9318aa4aae14
Alexander Shaposhnikov <ashaposhnikov@google.com>
2025-01-28 00:36:10 +0000

    6700 -> 6300

--- crypto/vm/boc.h
+++ crypto/vm/boc.h
@@ -124,7 +124,10 @@ struct CellStorageStat {
   struct CellInfo {
     td::uint32 max_merkle_depth = 0;
   };
-  std::unordered_map<vm::Cell::Hash, CellInfo> seen;
+  //std::unordered_map<vm::Cell::Hash, CellInfo> seen;
+
+  absl::flat_hash_map<vm::Cell::Hash, CellInfo> seen;
+
   CellStorageStat() : cells(0), bits(0), public_cells(0) {
   }
   explicit CellStorageStat(unsigned long long limit_cells)

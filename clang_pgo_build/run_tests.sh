5124f689f85e1977b88f16fb1cdec4ce06469413
Alexander Shaposhnikov <ashaposhnikov@google.com>
2025-01-28 06:12:50 +0000

    Use tcmalloc

--- clang_pgo_build/run_tests.sh
+++ clang_pgo_build/run_tests.sh
@@ -2,4 +2,5 @@
 export LD_LIBRARY_PATH=$HOME/my_llvm_fork/llvm-project/build/lib/x86_64-unknown-linux-gnu
 D=`realpath ../test_data/tests`
 echo "$D"
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so.4.5.16
 taskset -c 0-0 ./contest/grader/contest-grader --threads 1 --tests $D
